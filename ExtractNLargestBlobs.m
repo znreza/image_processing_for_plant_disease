f414dbbdbfcb92581efe380038e03b55b2042995
Zarreen Naowal Reza <zarreen.naowal.reza@gmail.com>
2017-12-28 22:19:22 -0500

    Add files via upload

---
+++ ExtractNLargestBlobs.m
@@ -0,0 +1,33 @@
+Reference of this function: https://www.mathworks.com/matlabcentral/fileexchange/55098-plant-leaf-disease-detection-and-classification-using-multiclass-svm-classifier?s_tid=prof_contriblnk
+
+function binaryImage = ExtractNLargestBlobs(binaryImage, numberToExtract)
+try
+	% Get all the blob properties.  Can only pass in originalImage in version R2008a and later.
+	[labeledImage, numberOfBlobs] = bwlabel(binaryImage);
+	blobMeasurements = regionprops(labeledImage, 'area');
+	% Get all the areas
+	allAreas = [blobMeasurements.Area];
+	if numberToExtract > 0
+		% For positive numbers, sort in order of largest to smallest.
+		% Sort them.
+		[sortedAreas, sortIndexes] = sort(allAreas, 'descend');
+	elseif numberToExtract < 0
+		% For negative numbers, sort in order of smallest to largest.
+		% Sort them.
+		[sortedAreas, sortIndexes] = sort(allAreas, 'ascend');
+		% Need to negate numberToExtract so we can use it in sortIndexes later.
+		numberToExtract = -numberToExtract;
+	else
+		% numberToExtract = 0.  Shouldn't happen.  Return no blobs.
+		binaryImage = false(size(binaryImage));
+		return;
+	end
+	% Extract the "numberToExtract" largest blob(a)s using ismember().
+	biggestBlob = ismember(labeledImage, sortIndexes(1:numberToExtract));
+	% Convert from integer labeled image into binary (logical) image.
+	binaryImage = biggestBlob > 0;
+catch ME
+	errorMessage = sprintf('Error in function ExtractNLargestBlobs().\n\nError Message:\n%s', ME.message);
+	fprintf(1, '%s\n', errorMessage);
+	uiwait(warndlg(errorMessage));
+end
